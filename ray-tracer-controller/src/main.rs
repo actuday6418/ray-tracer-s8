8727617381cc90bd23691cf793e60f1c9351e7e6
Arun Parolikkal <actuday6418@gmail.com>
2023-04-25 23:19:52 +0530

    doesn't build

--- ray-tracer-controller/src/main.rs
+++ ray-tracer-controller/src/main.rs
@@ -1,8 +1,38 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use crossbeam_channel::unbounded;
+use crossbeam_channel::{Receiver, Sender};
+use futures::future::join_all;
+use log::info;
+use pollster::block_on;
 use ray_tracer_interface::{
     color::Color,
     shapes::{mesh::Triangle, Object},
-    Point3, RenderInfo,
+    Point3, RenderInfo, RenderMeta,
 };
+use serde_json::json;
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
+use uuid::Uuid;
+
+enum MessageToWorker {
+    NewJob(Job),
+    Get(String),
+}
+
+struct Job {
+    id: Uuid,
+    obj: String,
+}
+
+enum MessageToServer {
+    Output(String),
+}
+
+struct AppState {
+    rx: Receiver<MessageToServer>,
+    tx: Sender<MessageToWorker>,
+    flag: Arc<RwLock<bool>>,
+}
 
 fn build_world() -> Vec<Object> {
     let mut world = vec![];
@@ -36,7 +66,7 @@ fn build_world() -> Vec<Object> {
                     (material.shininess as f32) / 1000f32,
                     Color::from_slice(material.diffuse),
                     0f32,
-                )))
+                )));
             }
         }
     } else {
@@ -45,14 +75,134 @@ fn build_world() -> Vec<Object> {
     world
 }
 
-fn main() {
-    let client = reqwest::blocking::Client::new();
-    let r = RenderInfo {
-        world: build_world(),
-    };
-    client
-        .post("http://0.0.0.0:8000")
-        .body(serde_json::to_string(&r).unwrap())
-        .send()
+async fn worker(
+    rx: Receiver<MessageToWorker>,
+    tx: Sender<MessageToServer>,
+    flag: Arc<RwLock<bool>>,
+) {
+    let mut results: HashMap<String, String> = HashMap::new();
+    loop {
+        info!("worker booted");
+        if let Ok(message) = rx.recv() {
+            match message {
+                MessageToWorker::NewJob(Job { id, obj: _ }) => {
+                    {
+                        let mut f = flag.write().unwrap();
+                        *f = true;
+                    }
+                    let client = reqwest::Client::new();
+                    let r = RenderInfo {
+                        world: build_world(),
+                        render_meta: RenderMeta::from_image_divisions(1920, 1080, 2),
+                        division_no: 0,
+                    };
+                    let slaves = vec![
+                        client
+                            .post("http://127.0.0.1:8081/")
+                            .header("Content-Type", "application/json")
+                            .body(json!(r).to_string())
+                            .send(),
+                        client
+                            .post("http://127.0.0.1:8081/")
+                            .header("Content-Type", "application/json")
+                            .body(
+                                json!(RenderInfo {
+                                    division_no: 1,
+                                    ..r
+                                })
+                                .to_string(),
+                            )
+                            .send(),
+                    ];
+                    let slaves: Vec<_> = join_all(slaves)
+                        .await
+                        .into_iter()
+                        .map(|x| x.unwrap().text())
+                        .collect();
+                    let img: String = join_all(slaves)
+                        .await
+                        .into_iter()
+                        .map(|x| x.unwrap())
+                        .fold(String::new(), |a, b| a + &b);
+                    results.insert(id.to_string(), img);
+                    {
+                        let mut f = flag.write().unwrap();
+                        *f = false;
+                    }
+                }
+                MessageToWorker::Get(id) => {
+                    let img = results.remove(&id).unwrap_or(String::new());
+                    tx.send(MessageToServer::Output(img)).unwrap();
+                }
+            }
+        }
+    }
+}
+
+#[post("/")]
+async fn index(req: web::Json<String>, state: web::Data<AppState>) -> impl Responder {
+    info!("Got request");
+    let req = req.into_inner();
+    let id = Uuid::new_v4();
+    {
+        let f = state.flag.read().unwrap();
+        if *f == true {
+            return String::from("We're busy");
+        }
+    }
+
+    state
+        .tx
+        .send(MessageToWorker::NewJob(Job { id, obj: req }))
         .unwrap();
+    id.to_string()
+}
+
+#[get("/poll/{id}")]
+async fn poll(id: web::Path<String>, state: web::Data<AppState>) -> impl Responder {
+    {
+        let f = state.flag.read().unwrap();
+        if *f == true {
+            return String::from("We're busy");
+        }
+    }
+    state
+        .tx
+        .send(MessageToWorker::Get(id.into_inner()))
+        .unwrap();
+    if let Ok(message) = state.rx.recv() {
+        match message {
+            MessageToServer::Output(img) => json!(img).to_string(),
+        }
+    } else {
+        String::new()
+    }
+}
+
+#[actix_web::main]
+async fn main() {
+    pretty_env_logger::init();
+    let (txw, rxw) = unbounded::<MessageToWorker>();
+    let (txs, rxs) = unbounded::<MessageToServer>();
+    let flag = Arc::new(RwLock::new(false));
+    let flag2 = flag.clone();
+    info!("Spawning worker!");
+    std::thread::spawn(|| block_on(worker(rxw, txs, flag2)));
+    let state = web::Data::new(AppState {
+        rx: rxs,
+        tx: txw,
+        flag,
+    });
+
+    HttpServer::new(move || {
+        App::new()
+            .service(index)
+            .service(poll)
+            .app_data(state.clone())
+    })
+    .bind(("127.0.0.1", 8080))
+    .unwrap()
+    .run()
+    .await
+    .unwrap();
 }
